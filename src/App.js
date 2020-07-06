01a77d16110e378e85210b16ddd22f495f1fdc15
Willian Rezende <wil-g2@hotmail.com>
2020-07-06 02:09:09 -0300

    feat: added App with react-paginate example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ReactPaginate from 'react-paginate';
+import Pagination from 'rc-pagination';
+import api from './services/api'; 
 
 function App() {
+  
+  const [products, setProducts] = useState([]);
+  
+  async function loadData() {
+    const response = await api.get('products?_page=1&_limit=5');
+    setProducts(response.data);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []); 
+
+  const handlePageClick = async (data) => { 
+    const response = await api.get(`products?_page=${data.selected+1}&_limit=5`);
+    setProducts(response.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Pagination className="ant-pagination" defaultCurrent={3} total={450} /> */}
+      <div>
+        { products && products.map( product => (
+          <div key={product.id}>{product.name}</div>
+        ))}
+      </div>
+      <ReactPaginate
+          previousLabel={'<'}
+          nextLabel={'>'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
+          pageCount={5}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+          initialPage={0}
+          disabledClassName={'disabled'}
+        />
     </div>
   );
 }
+
+/* 
+https://www.npmjs.com/package/rc-pagination
+https://www.npmjs.com/package/react-paginate
+
+*/ 
 
 export default App;
